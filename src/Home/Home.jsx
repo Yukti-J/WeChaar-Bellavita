377a5524a0f1a6c2d6fd4a0991e156ebfd906592
Mr-DarkFire <shivalsinha3@gmail.com>
2024-07-02 14:45:09 +0530

    update2

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,27 +6,27 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="w-[100%] bg-gradient-to-r from-[#8F0060] via-[#12161C] to-[#3E162C] overflow-hidden min-[640px]:h-screen h-[440px]">
+      <div className="w-[100%] bg-gradient-to-r from-[#8F0060] via-[#12161C] to-[#3E162C] overflow-hidden min-[640px]:h-screen h-[440px] classhome ">
         <Navbar
           search="bg-[#6D074C]"
           background="bg-gradient-to-r from-[#8F0060] via-[#12161C] to-[#3E162C]"
         />
-        <div className="flex flex-col justify-center items-center min-[510px]:mt-20 mt-40 min-[510px]:pb-36 pb-44 max-[510px]:h-full translate-y-[-60px]	">
+        <div className="flex flex-col justify-center items-center max-[1250px]: mt-10 min-[510px]:mt-20 mt-40 min-[510px]:pb-36 pb-44 max-[510px]:h-full translate-y-[-60px]	content">
           <p 
-          className="min-[1250px]:text-[80px] min-[1040px]:text-[70px] min-[740px]:text-[45px] min-[510px]:text-[30px] text-[25px] font-against text-[#FFE1F1] min-[510px]:mt-16 text-center">
+          className="min-[1250px]:text-[80px] min-[1040px]:mt-40 text-[70px] min-[740px]:text-[45px] min-[510px]:text-[30px] text-[25px] font-against text-[#FFE1F1] min-[510px]:mt-16 text-center t1">
             DISCOVER THE ART
           </p>
           <img
             src={HomePerfume}
             alt="HomePerfume"
-            className="min-[1040px]:h-[450px] min-[740px]:h-[350px] min-[510px]:h-[220px] h-[150px] min-[1040px]:-mt-52 min-[740px]:-mt-40 min-[610px]:-mt-28 -mt-14 z-10 opacity-95 perfume"
+            className="min-[1040px]:h-[450px] min-[740px]:h-[350px] min-[510px]:h-[220px] h-[150px] min-[1040px]:-mt-40 min-[740px]:-mt-40 min-[610px]:-mt-28 -mt-14 z-10 opacity-95 perfume i1"
           />
           <img
             src={HomePerfume}
             alt="HomePerfume"
-            className="min-[1040px]:h-[500px] min-[740px]:h-[400px] min-[510px]:h-[220px] h-[150px] min-[740px]:-mt-20 min-[510px]:-mt-20 -mt-10 z-0 opacity-20 -scale-y-90 blur-[1px]"
+            className="min-[1040px]:h-[500px] min-[740px]:h-[400px] min-[510px]:h-[220px] h-[150px] min-[740px]:-mt-20 min-[510px]:-mt-20 -mt-10 z-0 opacity-20 -scale-y-90 blur-[1px] i2"
           />
-          <p className="min-[1250px]:text-[120px] min-[1040px]:text-[100px] min-[740px]:text-[70px] min-[510px]:text-[48px] text-[38px] font-against text-[#f9efe3] min-[1040px]:-mt-[600px] min-[740px]:-mt-[460px] min-[510px]:-mt-[220px] -mt-[170px] z-20 drop-shadow-2xl">
+          <p className="min-[1250px]:text-[120px] min-[1040px]:text-[100px] min-[740px]:text-[70px] min-[510px]:text-[48px] text-[38px] font-against text-[#f9efe3] min-[1040px]:-mt-[600px] min-[740px]:-mt-[460px] min-[510px]:-mt-[220px] -mt-[170px] z-20 drop-shadow-2xl t1">
             OF PERFUMERY
           </p>
         </div>
--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -3,8 +3,9 @@ import LoaderLogo from '../assets/LoaderLogo.png'
 import "./Loader.css"
 const Loader = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
+    <div className='w-screen h-screen flex justify-center items-center text'>
       <img src={LoaderLogo} alt="loader" className='w-28 h-28 loader'/>
+      <h4>Unveil the essence that defines you.</h4>
     </div>
   )
 }
